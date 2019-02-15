1446f4371d9f4534e54d07c83540d40cb7c44792
Chris Gårdenberg <chga@multinet.se>
2019-02-15 08:25:21 +0100

    Loading the participants-odata-class.

--- eduadmin-api-classes.php
+++ eduadmin-api-classes.php
@@ -89,6 +89,8 @@ class EduAdmin_ODataHolder {
 	public $InterestRegistrations = null;
 	/** @var EduAdmin_OData_Locations */
 	public $Locations = null;
+	/** @var EduAdmin_OData_Participants */
+	public $Participants = null;
 	/** @var EduAdmin_OData_Personnel */
 	public $Personnel = null;
 	/** @var EduAdmin_OData_Persons */
